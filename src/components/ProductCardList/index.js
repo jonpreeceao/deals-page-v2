6aca11bd9d560a9c814174a3ab8736c66015841d
DRL\preecjo <jon.preece@ao.com>
2018-07-26 15:53:37 +0100

    Added additional sections

--- src/components/ProductCardList/index.js
+++ src/components/ProductCardList/index.js
@@ -6,6 +6,7 @@ class ProductCardList extends Component {
   render() {
     return (
       <div className={`${styles['c-product-card-list']} o-wrapper`}>
+        {this.props.header ? <h3>{this.props.header}</h3> : null}
         <div className={`${styles['c-product-card-list__inner']} o-flex-container`}>
           {this.props.products.map(product => (
             <ProductCard key={product.ProductIdentifier} productData={product} />
