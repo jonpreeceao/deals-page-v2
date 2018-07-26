06be6b04c2d6c5262b7602eb54d49b1554ae452e
DRL\oneilr <robin.oneill@ao.com>
2018-07-26 11:37:36 +0100

    [RO] Basic styles

--- src/components/ProductCardList/index.js
+++ src/components/ProductCardList/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import ProductCard from '../ProductCard'
+import styles from './styles'
 
 class ProductCardList extends Component {
   render() {
     return (
-      <div className="c-product-card-list">
-        {
-          this.props.products.map((product) => {
-            return <ProductCard key={product.ProductIdentifier} productData={product} />
-          })
-        }
+      <div className={styles['c-product-card-list']}>
+        <div className={styles['c-product-card-list__track']}>
+          {
+            this.props.products.map((product) => {
+              return <ProductCard key={product.ProductIdentifier} productData={product} />
+            })
+          }
+        </div>
       </div>
     )
   }
