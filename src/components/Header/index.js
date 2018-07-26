cdf37b502bba41db8043be562d2247dc938ab371
Craig Jones <craigdavidjones@hotmail.co.uk>
2018-07-26 11:37:22 +0100

    [CJ] Complete header (ISH)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React, { Component, Fragment } from 'react'
+import classnames from 'classnames'
 import styles from './styles.scss'
 
 class Header extends Component {
 
   render() {
+    var classes = classnames(
+      {[styles.dpPill]: true},
+      'c-btn c-btn--pill u-mr--tiny u-ml--tiny dp-pill'
+    )
     return (
       <Fragment>
         <header className={styles.header}>
@@ -15,10 +20,14 @@ class Header extends Component {
         </header>
         <div className={styles.inner}>
           <div className={styles.buttonContainer}>
-            <a className={styles.button}>Gifts Under £50</a>
-            <a className={styles.button}>Cooling Gifts</a>
-            <a className={styles.button}>Fathers Days</a>
-            <a className={styles.button}>Electronic Deals</a>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
+            <button className={classes}>Gifts Under £50</button>
           </div>
         </div>
       </Fragment>
