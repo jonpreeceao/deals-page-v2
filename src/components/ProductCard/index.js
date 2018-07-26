41c972818a104dc6c92f7a6da1498b2ef51c2a9e
James Almond <jamestalmond@gmail.com>
2018-07-26 15:21:32 +0100

    Styling updates on cards and lists

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -6,12 +6,14 @@ function ProductCard(props) {
   const { productData } = props;
 
   return (
-    <div className={styles['c-product-card-list__item']}>
-      <img src={`//media.ao.com/brandlogos/en-GB/${productData.Images.BrandFileName}`} alt={productData.Brand} />
-      <img src={`//media.ao.com/en-GB/Productimages/Images/rvSmall/${productData.Images.SmallFileName}`} alt={productData.Title} />
-      <div>&pound;{productData.Price}</div>
-      <h2>{ productData.Title }</h2>
-      <div>{ productData.Rating }</div>
+    <div className={`${styles['c-product-card-list__item']} o-flex-item`}>
+      <div className={`${styles['c-product-card-list__item-inner']}`}>
+        <img src={`//media.ao.com/brandlogos/en-GB/${productData.Images.BrandFileName}`} alt={productData.Brand} />
+        <img src={`//media.ao.com/en-GB/Productimages/Images/rvSmall/${productData.Images.SmallFileName}`} alt={productData.Title} />
+        <div>&pound;{productData.Price}</div>
+        <h2>{ productData.Title }</h2>
+        <div>{ productData.Rating }</div>
+      </div>
     </div>
   )
 }
--- src/components/ProductCardList/index.js
+++ src/components/ProductCardList/index.js
@@ -5,8 +5,8 @@ import styles from './styles'
 class ProductCardList extends Component {
   render() {
     return (
-      <div className="o-wrapper u-pr--tiny u-pl--tiny">
-        <div className={styles['c-product-card-list']}>
+      <div className={`${styles['c-product-card-list']} o-wrapper`}>
+        <div className={`${styles['c-product-card-list__inner']} o-flex-container`}>
           {this.props.products.map(product => (
             <ProductCard key={product.ProductIdentifier} productData={product} />
           ))}
