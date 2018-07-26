c56b6d9763755d47eb8e3b7208f82cb9d69768cc
DRL\oneilr <robin.oneill@ao.com>
2018-07-26 16:26:24 +0100

    [RO] Refactor heading style

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -4,11 +4,19 @@ import styles from './styles.scss'
 class Heading extends Component {
 
   render() {
+    let classString = styles['c-heading-divide'];
+    if (this.props.isDark === 'true') {
+      classString = classString + ' ' + styles['c-heading-divide--dark']
+    }
+
     return (
-      <div className={styles.inner}>
-        <h2 className={this.props.isDark === 'true' ? styles.linethroughDark : styles.linethroughLight}>
-          <span style={{ backgroundColor: this.props.bgColor }}>{this.props.heading}</span>
+      <div className="o-wrapper u-pr--tiny u-pl--tiny">
+        <h2 className={classString}>
+          {this.props.heading}
         </h2>
+        {/* <h2 className={this.props.isDark === 'true' ? styles.linethroughDark : styles.linethroughLight}>
+          <span style={{ backgroundColor: this.props.bgColor }}>{this.props.heading}</span>
+        </h2> */}
       </div>
     )
   }
